3238bd11df4a759b1ceb88ba4de50b7c80522dab
dev-DanielNascimento <danielcunha12.contato@gmail.com>
2026-04-26 16:46:33 -0300

    feat(limpezaDados): limpeza de dados v1

--- app.py
+++ app.py
@@ -6,3 +6,8 @@ st.set_page_config(page_title="Stude", page_icon="📚", layout="centered")
 con = inicia_conexao()
 
 st.title("📚 Stude")
+
+col_b, col_a = st.columns(2)
+
+#if submit_button:
+#    with conn.cursor() as cur:
--- limpeza_dados/limparDados.py
+++ limpeza_dados/limparDados.py
@@ -1,6 +1,11 @@
 import pandas as pd
 
 df = pd.read_csv('limpeza_dados/data.csv', encoding='utf8', skipinitialspace=True)
-df = df[]
-df = df.columns.str.strip()
-print(df.head())
+df.columns = df.columns.str.strip() # Limpa espaço das colunas
+df = df.drop(columns=['Propriedade', 'Fim', 'Hora', 'Start', 'StartBT', 'StopBT'])
+
+df['Pausas (min)'] = pd.to_numeric(df['Pausas (min)'], errors='coerce') 
+df['Pausas (min)'] = df['Pausas (min)'].astype('Int64')
+df['Pausas (min)'] = df['Pausas (min)'].fillna(0)
+
+print(df.head(50))
