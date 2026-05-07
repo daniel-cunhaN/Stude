dc4ddbd8f40a46b397077c3ed56200c42c72c58c
dev-DanielNascimento <danielcunha12.contato@gmail.com>
2026-05-06 21:00:26 -0300

    chore(horas_e_metas): Corrige função de agregação de horas que estava mostrando semana real em vez de semana do calendário

--- metodos/horas_e_metas.py
+++ metodos/horas_e_metas.py
@@ -6,23 +6,22 @@ con = iniciar_conexao()
 def agregar_horas(con):
     with con.cursor() as cur:
         # Horas feitas hoje
-        cur.execute("SELECT SUM(minutos) - SUM(pausas_min) FROM log_estudo WHERE data = CURRENT_DATE")
+        # O COALESCE garante que se não houver pausas, o SQL use 0 em vez de NULL
+        cur.execute("SELECT SUM(minutos - COALESCE(pausas_min, 0)) FROM log_estudo WHERE data = CURRENT_DATE")
         resultado_hoje = cur.fetchone()[0]
-        minutos_hoje = resultado_hoje if resultado_hoje else 0 #if e else para proteger conta horas_hoje caso retorne nulo
+        minutos_hoje = resultado_hoje if resultado_hoje else 0
         horas_hoje = int(minutos_hoje / 60)
         min_restantes_hoje = int(minutos_hoje % 60)
 
-
-        # Horas feitas na semana
-        cur.execute("SELECT SUM(minutos) - SUM(pausas_min) FROM log_estudo WHERE data >= CURRENT_DATE - INTERVAL '7 days'")
+        # Horas feitas na semana (Calendário: de segunda a hoje)
+        cur.execute("SELECT SUM(minutos - COALESCE(pausas_min, 0)) FROM log_estudo WHERE data >= date_trunc('week', CURRENT_DATE)")
         resultado_semana = cur.fetchone()[0]
         minutos_semana = resultado_semana if resultado_semana else 0
         horas_semana = int(minutos_semana / 60)
         min_restantes_semana = int(minutos_semana % 60)
 
-
-        # Horas feitas no mês
-        cur.execute("SELECT SUM(minutos) - SUM(pausas_min) FROM log_estudo WHERE date_trunc('month', data) = date_trunc('month', CURRENT_DATE)")
+        # Horas feitas no mês (Calendário: do dia 1 até hoje)
+        cur.execute("SELECT SUM(minutos - COALESCE(pausas_min, 0)) FROM log_estudo WHERE date_trunc('month', data) = date_trunc('month', CURRENT_DATE)")
         resultado_mes = cur.fetchone()[0]
         minutos_mes = resultado_mes if resultado_mes else 0
         horas_mes = int(minutos_mes / 60)
